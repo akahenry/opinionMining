7893d6e735a40a55e1054e2d16ca7e8ef90e30f4
Henry <38134284+akahenry@users.noreply.github.com>
2018-11-05 23:43:48 -0200

    FIX - Now, BST is working with strings.
    I've changed all the functions which use comparison with numbers to comparison with strings, to be possible sort them in a BST.
    I've also changed main.c to see how this is working.
    
    Changed functions:
    - BST_insertion
    - BST_remove
    - BST_consult
    - BST_print  // I didn't change this function, but its internal functions (BST_prefixedL, ...).

--- ADT_BST/ADT_BST.c
+++ ADT_BST/ADT_BST.c
@@ -10,19 +10,20 @@ BSTNode* BST_initialize(void)
   return NULL;
 }
 
-BSTNode* BST_insertion(BSTNode* tree, int info)
+BSTNode* BST_insertion(BSTNode* tree, char *info, int value)
 {
   if(tree)
   {
-    if(info >= tree->info)
-      tree->right = BST_insertion(tree->right, info);
+    if(strcmp(info,tree->info) > 0)
+      tree->right = BST_insertion(tree->right, info, value);
     else
-      tree->left = BST_insertion(tree->left, info);
+      tree->left = BST_insertion(tree->left, info, value);
   }
   else
   {
     tree = malloc(sizeof(BSTNode));
-    tree->info = info;
+    strcpy(tree->info, info);
+    tree->value = value;
     tree->right = NULL;
     tree->left = NULL;
   }
@@ -30,18 +31,18 @@ BSTNode* BST_insertion(BSTNode* tree, int info)
   return tree;
 }
 
-BSTNode* BST_remove(BSTNode* tree, int info)
+BSTNode* BST_remove(BSTNode* tree, char* info)
 {
   BSTNode* answer = tree;
   BSTNode* temp;
 
   if(tree)  // If tree != NULL.
   {
-    if(info > tree->info)   // Checking in the right sub-tree.
+    if(strcmp(info,tree->info) > 0)   // Checking in the right sub-tree.
     {
       tree->right = BST_remove(tree->right, info);
     }
-    else if(info < tree->info)    // Checking in the left sub-tree.
+    else if(strcmp(info,tree->info) < 0)    // Checking in the left sub-tree.
     {
       tree->left = BST_remove(tree->left, info);
     }
@@ -62,7 +63,8 @@ BSTNode* BST_remove(BSTNode* tree, int info)
       else    // tree has two sub-trees
       {
         temp = BST_maxNode(tree->left);   // Finding the maximum key node.
-        tree->info = temp->info;      // Putting the temp->info in the tree->info.
+        strcpy(tree->info, temp->info);   // Putting the temp->info in the tree->info.
+        tree->value = temp->value;
         tree->left = BST_remove(tree->left, temp->info);  // Deleting the node which has the same temp->info, as we already have changed the nodes.
         answer = tree;
       }
@@ -72,17 +74,17 @@ BSTNode* BST_remove(BSTNode* tree, int info)
   return answer;
 }
 
-BSTNode* BST_consult(BSTNode* tree, int info)
+BSTNode* BST_consult(BSTNode* tree, char* info)
 {
   BSTNode* answer = NULL;  // Initializing return.
 
   if(tree)  // If tree isn't a NULL pointer, it must check the info.
   {
-    if(info == tree->info)
+    if(strcmp(info,tree->info) == 0)
       answer = tree;   // answer = info
     else
     {
-      if(info > tree->info)   // If info is greater than info's tree, the answer will be the result of right consult.
+      if(strcmp(info,tree->info) > 0)   // If info is greater than info's tree, the answer will be the result of right consult.
         answer = BST_consult(tree->right, info);
       else                    // Else, it must be the result of left consult.
         answer = BST_consult(tree->left, info);
@@ -157,7 +159,7 @@ void BST_prefixedL(BSTNode* tree, int aux)
       for(i = 0; i < aux; i++)  // Printing the "stairs".
         printf("=");
 
-      printf("%d\n", tree->info);
+      printf("%s\n", tree->info);
 
       BST_prefixedL(tree->left, aux+1);
       BST_prefixedL(tree->right, aux+1);
@@ -165,7 +167,7 @@ void BST_prefixedL(BSTNode* tree, int aux)
   }
   else
   {
-    printf("%d\n", tree->info);
+    printf("%s\n", tree->info);
 
     BST_prefixedL(tree->left, aux);
     BST_prefixedL(tree->right, aux);
@@ -183,7 +185,7 @@ void BST_prefixedR(BSTNode* tree, int aux)
       for(i = 0; i < aux; i++)  // Printing the "stairs".
         printf("=");
 
-      printf("%d\n", tree->info);
+      printf("%s\n", tree->info);
 
       BST_prefixedR(tree->right, aux+1);
       BST_prefixedR(tree->left, aux+1);
@@ -191,7 +193,7 @@ void BST_prefixedR(BSTNode* tree, int aux)
   }
   else
   {
-    printf("%d\n", tree->info);
+    printf("%s\n", tree->info);
 
     BST_prefixedR(tree->right, aux);
     BST_prefixedR(tree->left, aux);
@@ -226,7 +228,7 @@ void BST_postfixedL(BSTNode* tree, int aux)
       for(i = 0; i < aux; i++)  // Printing the "stairs".
         printf("=");
 
-      printf("%d\n", tree->info);
+      printf("%s\n", tree->info);
     }
   }
   else
@@ -234,7 +236,7 @@ void BST_postfixedL(BSTNode* tree, int aux)
     BST_postfixedL(tree->left, aux);
     BST_postfixedL(tree->right, aux);
 
-    printf("%d\n", tree->info);
+    printf("%s\n", tree->info);
   }
 }
 
@@ -252,7 +254,7 @@ void BST_postfixedR(BSTNode* tree, int aux)
       for(i = 0; i < aux; i++)  // Printing the "stairs".
         printf("=");
 
-      printf("%d\n", tree->info);
+      printf("%s\n", tree->info);
     }
   }
   else
@@ -260,7 +262,7 @@ void BST_postfixedR(BSTNode* tree, int aux)
     BST_postfixedR(tree->right, aux);
     BST_postfixedR(tree->left, aux);
 
-    printf("%d\n", tree->info);
+    printf("%s\n", tree->info);
   }
 }
 
@@ -291,7 +293,7 @@ void BST_centerL(BSTNode* tree, int aux)
       for(i = 0; i < aux; i++)  // Printing the "stairs".
         printf("=");
 
-      printf("%d\n", tree->info);
+      printf("%s\n", tree->info);
 
       BST_centerL(tree->right, aux+1);
     }
@@ -300,7 +302,7 @@ void BST_centerL(BSTNode* tree, int aux)
   {
     BST_centerL(tree->left, aux);
 
-    printf("%d\n", tree->info);
+    printf("%s\n", tree->info);
 
     BST_centerL(tree->right, aux);
   }
@@ -319,7 +321,7 @@ void BST_centerR(BSTNode* tree, int aux)
       for(i = 0; i < aux; i++)  // Printing the "stairs".
         printf("=");
 
-      printf("%d\n", tree->info);
+      printf("%s\n", tree->info);
 
       BST_centerR(tree->left, aux+1);
     }
@@ -328,7 +330,7 @@ void BST_centerR(BSTNode* tree, int aux)
   {
     BST_centerR(tree->right, aux);
 
-    printf("%d\n", tree->info);
+    printf("%s\n", tree->info);
 
     BST_centerR(tree->left, aux);
   }
